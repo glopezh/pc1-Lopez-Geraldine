f52bc6f56f24f7d760c0a38ce0575ff601c2129c
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-09-01 03:03:19 +0200

    first commit

---
+++ src/main/java/com/cjava/clientes/Pc1LopezGeraldineApplication.java
@@ -0,0 +1,13 @@
+package com.cjava.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pc1LopezGeraldineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pc1LopezGeraldineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjava/clientes/Pc1LopezGeraldineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjava.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pc1LopezGeraldineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

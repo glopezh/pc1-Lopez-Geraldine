3caf52d2c92e05d0907123482ae76b94f43e583c
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-09-01 03:10:46 +0200

    Initial commit

--- src/main/java/com/cjava/clientes/Pc1LopezGeraldineApplication.java
+++ src/main/java/com/cjava/clientes/Pc1LopezGeraldineApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Pc1LopezGeraldineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Pc1LopezGeraldineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Pc1LopezGeraldineApplication.class, args);
+    }
 
 }
